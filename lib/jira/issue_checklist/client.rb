a9cefc5dc55d95e090ee71ca2fd74d8338d1e4cb
kei178 <keisuke.inaba.cpa@gmail.com>
2021-05-10 16:50:34 -0500

    Add jira api client fetch all pages

--- lib/jira/issue_checklist/client.rb
+++ lib/jira/issue_checklist/client.rb
@@ -9,6 +9,7 @@ module Jira
     class Client
       BASE_URL = 'https://herocoders.atlassian.net/rest/api/3'
       PROJECT_NAME = 'IC'
+      RESULTS_PER_PAGE = 50
 
       def components
         fetch("#{BASE_URL}/project/IC/components")
@@ -26,7 +27,7 @@ module Jira
           project='#{PROJECT_NAME}' AND component IN (#{component_names.join(', ')})
         JQL
 
-        fetch("#{BASE_URL}/search?jql=#{jql}")['issues']
+        fetch_all_pages("#{BASE_URL}/search?jql=#{jql}", 'issues')
       end
 
       private
@@ -35,6 +36,21 @@ module Jira
         uri = URI.parse(url)
         res = Net::HTTP.get(uri)
         JSON.parse(res)
+      end
+
+      def fetch_all_pages(url, target_field)
+        results = []
+        start_at = 0
+        loop do
+          res = fetch("#{url}&startAt=#{start_at}&maxResults=#{RESULTS_PER_PAGE}")
+          total ||= res['total']
+          results += res[target_field]
+          break if results.count >= total
+
+          start_at = results.count
+        end
+
+        results
       end
     end
   end
